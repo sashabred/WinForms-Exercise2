dea1ab975619413885014bf8b9009ae4d8fc4ce1
Alexandra Sandanova <slowpoche@gmail.com>
2022-01-16 16:45:39 +0800

    Add project files.

---
+++ Exercise2/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Exercise2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnyes = new System.Windows.Forms.Button();
+            this.btnno = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnyes
+            // 
+            this.btnyes.Location = new System.Drawing.Point(36, 70);
+            this.btnyes.Name = "btnyes";
+            this.btnyes.Size = new System.Drawing.Size(75, 23);
+            this.btnyes.TabIndex = 0;
+            this.btnyes.Text = "Да";
+            this.btnyes.UseVisualStyleBackColor = true;
+            this.btnyes.Click += new System.EventHandler(this.btnyes_Click);
+            // 
+            // btnno
+            // 
+            this.btnno.Location = new System.Drawing.Point(130, 70);
+            this.btnno.Name = "btnno";
+            this.btnno.Size = new System.Drawing.Size(75, 23);
+            this.btnno.TabIndex = 1;
+            this.btnno.Text = "Нет";
+            this.btnno.UseVisualStyleBackColor = true;
+            this.btnno.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnno_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Вы довольны своей зарплатой?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 173);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnno);
+            this.Controls.Add(this.btnyes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Насущный вопрос";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnyes;
+        private System.Windows.Forms.Button btnno;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TestList/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace TestList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.memberList = new System.Windows.Forms.CheckedListBox();
+            this.peopleList = new System.Windows.Forms.ComboBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(176, 25);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Список участников";
+            // 
+            // memberList
+            // 
+            this.memberList.FormattingEnabled = true;
+            this.memberList.Location = new System.Drawing.Point(23, 43);
+            this.memberList.Name = "memberList";
+            this.memberList.Size = new System.Drawing.Size(238, 89);
+            this.memberList.TabIndex = 1;
+            // 
+            // peopleList
+            // 
+            this.peopleList.FormattingEnabled = true;
+            this.peopleList.Items.AddRange(new object[] {
+            "Санданова",
+            "Иванов",
+            "Шоболова",
+            "Терюханов"});
+            this.peopleList.Location = new System.Drawing.Point(23, 138);
+            this.peopleList.Name = "peopleList";
+            this.peopleList.Size = new System.Drawing.Size(238, 24);
+            this.peopleList.TabIndex = 2;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(276, 43);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(90, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(276, 85);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(90, 23);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(276, 124);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(99, 23);
+            this.buttonSort.TabIndex = 3;
+            this.buttonSort.Text = "Сортировка";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 249);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.peopleList);
+            this.Controls.Add(this.memberList);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Работа со списками";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckedListBox memberList;
+        private System.Windows.Forms.ComboBox peopleList;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSort;
+    }
+}
+
