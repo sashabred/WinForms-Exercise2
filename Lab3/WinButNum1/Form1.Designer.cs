555b2b0d566187c268476d9183b4cf3bc41469ae
Alexandra Sandanova <slowpoche@gmail.com>
2022-02-02 02:23:22 +0800

    New

---
+++ Lab3/WinButNum1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace WinButNum1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickButton1 = new WinButNum1.ClickButton();
+            this.SuspendLayout();
+            // 
+            // clickButton1
+            // 
+            this.clickButton1.Location = new System.Drawing.Point(111, 123);
+            this.clickButton1.Name = "clickButton1";
+            this.clickButton1.Size = new System.Drawing.Size(231, 106);
+            this.clickButton1.TabIndex = 0;
+            this.clickButton1.Text = "clickButton1";
+            this.clickButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clickButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ClickButton clickButton1;
+    }
+}
+
---
+++ Lab3/WinTImer1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace WinTImer1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControlTimer1 = new WinTImer1.UserControlTimer();
+            this.SuspendLayout();
+            // 
+            // userControlTimer1
+            // 
+            this.userControlTimer1.Location = new System.Drawing.Point(34, 27);
+            this.userControlTimer1.Name = "userControlTimer1";
+            this.userControlTimer1.Size = new System.Drawing.Size(150, 150);
+            this.userControlTimer1.TabIndex = 0;
+            this.userControlTimer1.TimeEnabled = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userControlTimer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControlTimer userControlTimer1;
+    }
+}
+
